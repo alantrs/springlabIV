22961c7ad6087a7cb912e05a5464ca0a950c52ca
Alan Araujo Rodrigues <88983038+alantrs@users.noreply.github.com>
2023-03-14 10:09:43 -0300

    Add files via upload

---
+++ src/main/java/br/gov/sp/fatec/springlabIV/SpringLabIvApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springlabIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabIvApplication.class, args);
+	}
+
+}
